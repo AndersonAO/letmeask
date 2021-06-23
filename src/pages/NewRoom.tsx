92d1fb93b1847d2a58bbed0d84e266c3520cde9f
Anderson Oliveira <whollgirau@gmail.com>
2021-06-22 22:48:40 -0300

    criando pagina de salas, adicionando funcionalidades de perguntas

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import {FormEvent, useState} from 'react'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
@@ -8,23 +9,46 @@ import { Button } from '../components/Button'
 
 import '../styles/auth.scss'
 import { useAuth } from '../hooks/useAuth'
+import { database } from '../services/firebase'
 export function NewRoom(){
   const { user } = useAuth()
+  const history = useHistory()
+  const [newRoom, setNewRoom] = useState('');
+
+  async function handleCreateRoom(event: FormEvent){
+    event.preventDefault()
+    
+    if(newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+
+    history.push(`/rooms/${firebaseRoom.key}`)
+  }
+
   return (
     <div id="page-auth">
       <aside>
         <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Cire salas de Q&amp;A ao-vivo</strong>
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tie as duvidas de sua audiência em tempo-real</p>
       </aside>
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
+          <form onSubmit={handleCreateRoom}>
             <input 
               type="text" 
               placeholder="Nome da sala"
+              onChange={e=> setNewRoom(e.target.value)}
+              value={newRoom}
             />
             <Button type="submit">
               Criar sala
